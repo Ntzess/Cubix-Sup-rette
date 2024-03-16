654a38b52b6647440b84ff148f585ca347593c32
Cubix Développement <133794305+Ntzess@users.noreply.github.com>
2024-03-16 01:31:39 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,44 @@
+Config = {
+
+    -- [FR] https://htmlcolorcodes.com/fr/ Site pour les couleur / [EN] https://htmlcolorcodes.com/fr/ Site for Colors --
+
+ColorMenuR = 39, -- [FR] Couleur du menu RGB R / [EN] RGB R Menu Color
+ColorMenuG = 255, -- [FR] Couleur du menu RGB G / [EN] RGB G Menu Color
+ColorMenuB = 0, -- [FR] Couleur du menu RGD B / [EN] RGB B Menu Color
+-- [FR] Nom Du Menu est Description Du Menu / [EN] Menu Name and Menu Description
+Menu = "Supérette",
+MenuDescription = "Menu Supérette", -- [FR] Description Du Menu / [EN] Menu Description
+Markers = 6, -- [FR] Type du marker afficher sur le sol / [EN] Menu separator it is displayed above the buttons
+OpacityMarkers1 = 1.0,
+OpacityMarkers2 = 1.0,
+OpacityMarkers3 = 1.0,
+ColorsMarkers1 = 39,
+ColorsMarkers2 = 255,
+ColorsMarkers3 = 0,
+OpacityColorsMarkers = 255,
+MarkersD = "Appuyez sur ~g~[E] ~s~pour ouvrir le menu",
+Blips = {
+    Scale = 0.8, -- [FR] Taille du blips / [EN] Blips Size
+    Color = 2, -- [FR] Couleur du blips / [EN] Blips Color
+    Sprite = 52, -- [FR] Icon du blips / [EN] Blips Icon
+    },   
+}
+
+Position = {
+    positionsuperette = {
+        {pos =vector3(25.8809, -1347.6716, 28.497)},
+        {pos =vector3(-707.5637, -914.6002, 18.2156)},
+        {pos =vector3(1136.1255, -982.2365, 45.4158)},
+        {pos =vector3(1163.123, -323.9778, 68.2051)},
+        {pos =vector3(374.1903, 325.9832, 102.5663)},
+   },
+}
+
+PrixSuperette = {
+    superetteprix = {
+       {name = "~h~Pain", prix = 10, futo = "bread", prixaffiche = "10$"}  ,
+       {name = "~h~Eau", prix = 10, futo = "water", prixaffiche = "10$"} ,
+       {name = "~h~Téléphone", prix = 950, futo = "phone", prixaffiche = "950$"},
+   },
+}
+
